b047bbc2efc507b8408c601d22f133d8595c1b08
Matheus <60353870+McarvalhoM@users.noreply.github.com>
2020-02-01 20:15:24 -0300

    Create 14.py

---
+++ 14.py
@@ -0,0 +1,15 @@
+#se joao pescar mais de 50kg, paga multa de 4 reais por kg em excesso
+#>> mostrar quantos kg de peixe total
+#>> mostrar quantos kg de excesso
+#>> mostrar quanto de multa joao ira pagar
+
+pescado= float(input('Digite o valor que Joao pescou'))
+if pescado <= 50:
+    print('joao pescou', pescado, 'kg de peixe')
+    print('joao nao pescou em excesso, portanto, nao pagara multa')
+elif pescado > 50:
+    excesso= float((pescado - 50))
+    multa= (excesso*4)
+    print('joao pescou', pescado, 'kg de peixe')
+    print('joao pescou', excesso, 'kg de peixe em excesso')
+    print('joao ira pagar', multa, 'R$ de multa')
